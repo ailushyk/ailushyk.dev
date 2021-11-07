b018236c3d1508aeb0b7d07ab09be9db4b12962e
Alex I <ailushyk@gmail.com>
2021-11-07 16:38:50 +0100

    CU-1nur0ra move to mf space (#13)
    * move next blog to apps
    
    * setup mf-space

--- apps/nextjs/pages/posts/index.tsx
+++ apps/nextjs/pages/posts/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import { Layout } from 'components/Layout'
+import { Layout } from '@/components/Layout'
 import { DateView } from '@/components/DateView'
 import utilStyles from '@/styles/utils.module.css'
 import { RichText } from 'prismic-reactjs'
