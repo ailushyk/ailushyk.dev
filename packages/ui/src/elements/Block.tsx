0cc147f059287e91610ff6a3bc5bce24ceb41af7
Alex Ilushyk <ailushyk@gmail.com>
2021-11-14 01:50:00 +0100

    work on storybook + tailwindcss

--- packages/ui/src/elements/Block.tsx
+++ packages/ui/src/elements/Block.tsx
@@ -4,6 +4,14 @@ export interface BlockProps extends HTMLProps<HTMLDivElement> {
   as?: 'div';
 }
 
+type TClasses = string | undefined;
+const mergeClasses = (a: TClasses = '', b: TClasses = '') => {
+  const set = new Set(a.split(' ').concat(b.split(' ')));
+  return [...set].join(' ').trim();
+};
+
 export const Block: FC<BlockProps> = ({ as = 'div', children, ...props }) => {
-  return React.createElement(as, props, children);
+  const classes = 'm-3 p-3';
+  const className = mergeClasses(classes, props.className);
+  return React.createElement(as, { ...props, className }, children);
 };
--- packages/ui/src/stories/elements/Block.stories.tsx
+++ packages/ui/src/stories/elements/Block.stories.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Block } from '../../elements';
+import { Block, BlockProps } from '../../elements';
 import { Meta, Story } from '@storybook/react';
-import { BlockProps } from '../../elements/Block';
 
 const Template: Story<BlockProps> = (args) => <Block {...args}>test</Block>;
 const Default = Template.bind({});
 
-export { Default };
+const Custom = Template.bind({});
+Custom.args = {
+  className: 'box-border border-solid border-4 border-light-blue-500',
+};
+
+export { Default, Custom };
 
 export default {
   component: Block,
