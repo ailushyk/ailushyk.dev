349af22be92a4e4d65954459de1c00811f0fb219
Alex I <ailushyk@gmail.com>
2021-11-20 01:39:25 +0100

    chore(build): setup production mode (#20)
    CU-1u3zbdt

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     enabled: false,
     content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
